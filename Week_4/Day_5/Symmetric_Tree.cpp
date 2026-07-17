8596b52c02c7dc67b6d6512986703b53ffbe3b25
rachit777-mishra <rachitme111@gmail.com>
2026-07-18 00:24:36 +0530

    Create Symmetric_Tree.cpp

---
+++ Week_4/Day_5/Symmetric_Tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+    class Solution {
+    public:
+    bool mirror(TreeNode* p, TreeNode* q)
+    {
+        if(p == NULL && q == NULL)
+        {
+            return true;
+        }
+
+        if(p == NULL || q == NULL)
+        {
+            return false;
+        }
+
+        if(p->val != q->val)
+        {
+            return false;
+        }
+
+        return mirror(p->left, q->right) &&
+               mirror(p->right, q->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL)
+        {
+            return true;
+        }
+
+        return mirror(root->left, root->right);
+    }
+};
